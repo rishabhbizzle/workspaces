f3ffa845a5a8231daeda20d8d3235d034546e2b1
rishabhbizzle <rishabhbizzle@gmail.com>
2024-02-01 21:15:02 +0530

    refactor: dashboard page formatting

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,38 +1,40 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import React from 'react'
-import { cookies } from 'next/headers'
-import db from '@/lib/supabase/db'
-import DashboardSetup from '@/components/dashboard/dashboard-setup'
-import { getUserSubscriptionStatus } from '@/lib/supabase/queries'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import React from "react";
+import { cookies } from "next/headers";
+import db from "@/lib/supabase/db";
+import DashboardSetup from "@/components/dashboard/dashboard-setup";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
-  const supabase = createServerComponentClient( { cookies })
-  const {data : { user }} = await supabase?.auth?.getUser()
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase?.auth?.getUser();
 
   if (!user) return;
 
   const workspace = await db.query.workspaces.findFirst({
-    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id)
-  })
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
 
-  const  { data: subscription, error: subscriptionError } = await getUserSubscriptionStatus(user.id)
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
 
   if (subscriptionError) return;
 
-  if (!workspace) return(
-    <div
-    className='bg-background h-screen w-screen flex justify-center items-center'
-    >
-      <DashboardSetup user={user} subscription={subscription}></DashboardSetup>
-    </div>
+  if (!workspace)
+    return (
+      <div className="bg-background h-screen w-screen flex justify-center items-center">
+        <DashboardSetup
+          user={user}
+          subscription={subscription}
+        ></DashboardSetup>
+      </div>
+    );
+  redirect(`/dashboard/${workspace.id}`);
 
+  return <div>age</div>;
+};
 
-  )
-
-
-  return (
-    <div>age</div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
